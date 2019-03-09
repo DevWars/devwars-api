1a3c1e73844492e3be059c5bca70d6deb0c5856d
SYNTAG <accounts@blucitrus.com>
2019-03-08 20:19:01 -0700

    Increase bcrypt rounds

--- app/utils/hash.ts
+++ app/utils/hash.ts
@@ -1,6 +1,6 @@
 import * as bcrypt from "bcrypt";
 
-const NUM_ROUNDS = 4;
+const NUM_ROUNDS = 12;
 
 export async function hash(input: string): Promise<string> {
     return bcrypt.hash(input, NUM_ROUNDS);
