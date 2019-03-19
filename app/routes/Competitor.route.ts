01b02d6d6ebff37803cbfe1354a418b91962920b
SYNTAG <accounts@blucitrus.com>
2019-03-19 04:00:44 -0700

    Remove embeddables and Competitor

--- app/routes/Competitor.route.ts
+++
@@ -1,7 +0,0 @@
-import * as express from 'express';
-import {CompetitorController} from '../controllers/user/Competitor.controller';
-import {mustOwnUser} from '../middlewares/OwnsUser';
-
-export const CompetitorRoute: express.Router = express.Router()
-    .get('/:user/competitor', mustOwnUser, CompetitorController.forUser)
-    .post('/:user/competitor', mustOwnUser, CompetitorController.create);
