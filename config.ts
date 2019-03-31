0d3d4742cf8fa7642035e439465e972e6efb1d1d
f3ltron <fgiraud@student.42.fr>
2019-03-31 01:04:21 +0100

    feat(test): changing test config logic and first back

--- config.ts
+++ config.ts
@@ -5,7 +5,7 @@ export const DIALECT = 'postgres';
 const LOCAL_CONFIGURATION = {
     SERVER: env.SERVER || '127.0.0.1',
     PORT: Number(env.PORT_DB) || 5432,
-    DB: env.DB || 'devwars',
+    DB: env.DB || 'devwars_local', // TODO: maybe here it should devwars_local
     USER: env.USER_DB || 'postgres',
     PASSWORD: env.PASSWORD || 'secret',
 };
@@ -13,13 +13,28 @@ const LOCAL_CONFIGURATION = {
 const PRODUCTION_CONFIGURATION = {
     SERVER: env.SERVER || 'localhost',
     PORT: Number(env.PORT_DB) || 5432,
-    DB: env.DB || 'devwars',
+    DB: env.DB || 'devwars', // TODO: maybe here it should devwars_production
     USER: env.USER_DB || 'postgres',
     PASSWORD: env.PASSWORD || 'secret',
 };
 
+const TEST_CONFIGURATION = {
+    SERVER: env.SERVER || 'localhost',
+    PORT: Number(env.PORT_DB) || 5433,
+    DB: env.DB || 'devwars_test',
+    USER: env.USER_DB || 'postgres',
+    PASSWORD: env.PASSWORD || 'secret',
+}
+
+
+const mask: any = {
+    'LOCAL': LOCAL_CONFIGURATION,
+    'PRODUCTION': PRODUCTION_CONFIGURATION,
+    'test': TEST_CONFIGURATION
+}
+
 export const config = {
-    DATABASE: env.NODE_ENV === 'PRODUCTION' ? PRODUCTION_CONFIGURATION : LOCAL_CONFIGURATION,
+    DATABASE: mask[env.NODE_ENV] || mask.LOCAL,
     PORT_APP: 8080,
     SECRET: 'HltH3R3',
 };
