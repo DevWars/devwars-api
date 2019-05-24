f643a2c5f1fa5cae8b0dc16386c8d709079791f9
Stephen Lineker-Miller <slinekermiller@gmail.com>
2019-05-24 07:55:42 +0100

    Seed transactions and logging
    The seeding process now contains the transactions to help keep
    a constant stable state in the database.
    
     * Provided simples logs for understanding seed process.

--- cli/Seeder.ts
+++ cli/Seeder.ts
@@ -1,91 +1,93 @@
 import * as typeorm from 'typeorm';
 
-import { Connection } from '../config/Database';
-
-import UserFactory from '../app/factory/User.factory';
-import UserProfileFactory from '../app/factory/UserProfile.factory';
-import GameFactory from '../app/factory/Game.factory';
-import GameScheduleFactory from '../app/factory/GameSchedule.factory';
 import GameApplicationFactory from '../app/factory/GameApplication.factory';
-
+import GameScheduleFactory from '../app/factory/GameSchedule.factory';
+import UserProfileFactory from '../app/factory/UserProfile.factory';
 import UserStatsFactory from '../app/factory/UserStats.factory';
+import GameFactory from '../app/factory/Game.factory';
+import UserFactory from '../app/factory/User.factory';
 
-import User from '../app/models/User';
+import { Connection } from '../config/Database';
 import { UserRole } from '../app/models/User';
 
-import UserRepository from '../app/repository/User.repository';
 import GameScheduleRepository from '../app/repository/GameSchedule.repository';
 import UserGameStatsFactory from '../app/factory/UserGameStats.factory';
+import UserRepository from '../app/repository/User.repository';
 import ActivityFactory from '../app/factory/Activity.factory';
 
 let connection: typeorm.Connection;
+let connectionManager: typeorm.EntityManager;
 
 const generateConstantUsers = async () => {
     for (const role of ['admin', 'moderator', 'user']) {
         const user = UserFactory.withUsername(`test-${role}`);
         user.role = (UserRole as any)[role.toUpperCase()];
-        const newUser = await user.save();
 
-        const profile = UserProfileFactory.default();
-        profile.user = newUser;
-        await connection.manager.save(profile);
+        await connectionManager.transaction(async (transcation) => {
+            await transcation.save(user);
 
-        const stats = UserStatsFactory.default();
-        stats.user = newUser;
-        await connection.manager.save(stats);
+            const profile = UserProfileFactory.default();
+            const stats = UserStatsFactory.default();
+
+            profile.user = user;
+            stats.user = user;
+
+            await transcation.save(profile);
+            await transcation.save(stats);
+        });
     }
 };
 
 const generateBasicUsers = async () => {
     await generateConstantUsers();
 
-    for (let i = 4; i < 100; i++) {
-        const user = UserFactory.default();
-        const newUser = await connection.manager.save(user);
+    for (let i = 4; i <= 100; i++) {
+        await connectionManager.transaction(async (transaction) => {
+            const profile = UserProfileFactory.default();
+            const stats = UserStatsFactory.default();
+            const user = UserFactory.default();
 
-        const profile = UserProfileFactory.default();
-        profile.user = newUser;
-        await connection.manager.save(profile);
+            await transaction.save(user);
 
-        const stats = UserStatsFactory.default();
-        stats.user = newUser;
-        await connection.manager.save(stats);
+            profile.user = user;
+            stats.user = user;
 
-        await generateActivitiesForUser(user);
-    }
-};
+            await transaction.save(profile);
+            await transaction.save(stats);
 
-const generateActivitiesForUser = async (user: User) => {
-    for (let i = 1; i < 25; i++) {
-        const activity = ActivityFactory.withUser(user);
-
-        await connection.manager.save(activity);
+            for (let j = 1; j <= 25; j++) {
+                const activity = ActivityFactory.withUser(user);
+                await transaction.save(activity);
+            }
+        });
     }
 };
 
 const generateGames = async () => {
-    for (let i = 1; i < 50; i++) {
-        const schedule = GameScheduleFactory.default();
-        const newSchedule = await connection.manager.save(schedule);
+    const userRepository = await typeorm.getCustomRepository(UserRepository);
 
-        const game = await GameFactory.default();
-        newSchedule.game = await connection.manager.save(game);
-        await connection.manager.save(newSchedule);
+    for (let i = 1; i <= 50; i++) {
+        await connectionManager.transaction(async (transaction) => {
+            const gameStats = UserGameStatsFactory.default();
+            const schedule = GameScheduleFactory.default();
+            schedule.game = await GameFactory.default();
 
-        const userRepository = await typeorm.getCustomRepository(UserRepository);
-        const gameStats = UserGameStatsFactory.default();
-        gameStats.user = await userRepository.findOne(i);
-        await connection.manager.save(gameStats);
+            await transaction.save(schedule.game);
+            await transaction.save(schedule);
+
+            gameStats.user = await userRepository.findOne(i);
+            await transaction.save(gameStats);
+        });
     }
 };
 
 const generateApplications = async () => {
-    for (let i = 1; i < 25; i++) {
-        const userRepository = await typeorm.getCustomRepository(UserRepository);
-        const user = await userRepository.findOne(i);
+    const gameScheduleRepository = await typeorm.getCustomRepository(GameScheduleRepository);
+    const userRepository = await typeorm.getCustomRepository(UserRepository);
 
-        const gameScheduleRepository = await typeorm.getCustomRepository(GameScheduleRepository);
-        const schedule = await gameScheduleRepository.findOne(user.id);
+    for (let i = 1; i <= 25; i++) {
+        const schedule = await gameScheduleRepository.findOne(i);
+        const user = await userRepository.findOne(i);
 
         const application = GameApplicationFactory.withScheduleAndUser(schedule, user);
         await connection.manager.save(application);
@@ -94,17 +96,21 @@ const generateApplications = async () => {
 
 (async () => {
     connection = await Connection;
+    connectionManager = typeorm.getManager(connection.name);
 
+    console.log('Seeding database');
+    console.log('Synchronizing database, dropTablesBeforeSync = true');
     await connection.synchronize(true);
 
+    console.log('Generating basic users');
     await generateBasicUsers();
 
+    console.log('Generating games');
     await generateGames();
+
+    console.log('Generating applications');
     await generateApplications();
 
-    const userRepository = await typeorm.getCustomRepository(UserRepository);
-    const user = await userRepository.findOne(5);
-    user.profile = await userRepository.findProfileByUser(user);
-
+    console.log('Seeding complete');
     await connection.close();
 })();
