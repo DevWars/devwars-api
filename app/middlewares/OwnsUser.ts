a84352ea8b1eb160321dbc0a47f6787b96141c05
Stephen Lineker-Miller <slinekermiller@gmail.com>
2019-10-03 21:02:38 +0100

    maint(OwnsUser): ensures using mustBeAuthenticated instead of checking independently
    Since OwnsUser was ensuring that the current user was also authenticated. Use the already existing middleware instead of self validating.

--- app/middlewares/OwnsUser.ts
+++ app/middlewares/OwnsUser.ts
@@ -1,17 +1,21 @@
-import { NextFunction, Request, Response } from 'express';
-import { getCustomRepository } from 'typeorm';
+import { NextFunction, Response } from 'express';
 
+import { IRequest } from '../request/IRequest';
 import { UserRole } from '../models/User';
-import UserRepository from '../repository/User.repository';
 
-export const mustOwnUser = async (request: Request, response: Response, next: NextFunction) => {
-    const token = request.cookies.token;
-    const userRepository = getCustomRepository(UserRepository);
-    const user = await userRepository.findByToken(token);
-
+/**
+ *  mustOwnUser ensures that the current authenticted is the same entity as the one the following
+ *  request is being performed on. e.g updating there own profile but not owners.
+ */
+export const mustOwnUser = async (request: IRequest, response: Response, next: NextFunction) => {
     const requestedUserId = Number(request.params.id);
-    if (user && user.id !== requestedUserId && user.role !== UserRole.ADMIN) {
-        return response.sendStatus(401);
+
+    // Ensure that the requesting user is the entity they are also trying to perfrom the following
+    // request on. For example: you can only update your own profile and not others (unless your a admin).
+    if (request.user.id !== requestedUserId && request.user.role !== UserRole.ADMIN) {
+        return response.sendStatus(401).json({
+            message: 'Unauthorized, you can only perfrom this action on youserlf.',
+        });
     }
 
     return next();
--- app/routes/Auth.routes.ts
+++ app/routes/Auth.routes.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+
 import { AuthController } from '../controllers/auth/Auth.controller';
 import { mustOwnUser } from '../middlewares/OwnsUser';
 import { asyncErrorHandler } from './handlers';
@@ -13,6 +14,10 @@ export const AuthRoute: express.Router = express
     .post('/register', asyncErrorHandler(AuthController.register))
     .get('/verify', asyncErrorHandler(AuthController.verify))
     .post('/reverify', mustBeAuthenticated, asyncErrorHandler(AuthController.reVerify))
-    .post('/forgot/password', mustOwnUser, asyncErrorHandler(AuthController.initiatePasswordReset))
-    .post('/reset/password', mustOwnUser, asyncErrorHandler(AuthController.resetPassword))
-    .post('/reset/email', mustOwnUser, asyncErrorHandler(AuthController.initiateEmailReset));
+    .post(
+        '/forgot/password',
+        [mustBeAuthenticated, mustOwnUser],
+        asyncErrorHandler(AuthController.initiatePasswordReset)
+    )
+    .post('/reset/password', [mustBeAuthenticated, mustOwnUser], asyncErrorHandler(AuthController.resetPassword))
+    .post('/reset/email', [mustBeAuthenticated, mustOwnUser], asyncErrorHandler(AuthController.initiateEmailReset));
