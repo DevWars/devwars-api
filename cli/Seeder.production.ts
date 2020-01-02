57d4985d716840a031194b56f54ae39f7ff977ce
Stephen Lineker-Miller <slinekermiller@gmail.com>
2020-01-02 14:08:51 +0000

    fix(seed-production): dont create the user again if it already exists

--- cli/Seeder.production.ts
+++ cli/Seeder.production.ts
@@ -1,11 +1,13 @@
 import * as typeorm from 'typeorm';
 import { randomBytes } from 'crypto';
+import { isNil } from 'lodash';
 
 import User, { UserRole } from '../app/models/User';
 import UserProfile from '../app/models/UserProfile';
 import UserStats from '../app/models/UserStats';
 import UserGameStats from '../app/models/UserGameStats';
 
+import UserRepository from '../app/repository/User.repository';
 import { Connection } from '../app/services/Connection.service';
 import { COMPETITOR_USERNAME } from '../app/constants';
 import logger from '../app/utils/logger';
@@ -29,7 +31,7 @@ async function generateRandomPassword(): Promise<string> {
 }
 
 const generateConstantUsers = async () => {
-    for (const user of [{ username: COMPETITOR_USERNAME, email: 'contact' }]) {
+    for (const user of [{ username: COMPETITOR_USERNAME, email: 'competitor' }]) {
         const password = await generateRandomPassword();
 
         const constantUser = new User(user.username, await hash(password), `${user.email}@devwars.tv`, UserRole.USER);
@@ -42,6 +44,12 @@ const generateConstantUsers = async () => {
         const gameStats = new UserGameStats(constantUser);
 
         await connectionManager.transaction(async (transactionalEntityManager) => {
+            const userRepository = transactionalEntityManager.getCustomRepository(UserRepository);
+            const exists = await userRepository.findByUsernameOrEmail(user.username, user.email);
+
+            // Don't create the constant user if the user already exists.
+            if (!isNil(exists)) return;
+
             await transactionalEntityManager.save(constantUser);
             await transactionalEntityManager.save(profile);
             await transactionalEntityManager.save(userStats);
@@ -54,11 +62,11 @@ const generateConstantUsers = async () => {
     connection = await Connection;
     connectionManager = typeorm.getManager(connection.name);
 
-    logger.info('Seeding database');
+    logger.info('Seeding database for production');
     logger.info('Synchronizing database, dropTablesBeforeSync = false');
     await connection.synchronize(false);
 
-    logger.info('Generating basic users');
+    logger.info('Generating constant users');
     await generateConstantUsers();
 
     logger.info('Seeding complete');
