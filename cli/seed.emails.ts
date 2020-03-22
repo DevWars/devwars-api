15f907908147f3dcc2633a52d98ac29de8564279
Stephen Lineker-Miller <slinekermiller@gmail.com>
2020-03-22 16:46:57 +0000

    feat: You can now call to seed the emails to something random

---
+++ cli/seed.emails.ts
@@ -0,0 +1,28 @@
+import { Connection as typeConnection } from 'typeorm';
+import * as faker from 'faker';
+
+import { Connection } from '../app/services/Connection.service';
+import logger from '../app/utils/logger';
+import User from '../app/models/User';
+
+let connection: typeConnection;
+
+const updateEmailAddress = async () => {
+    const users = await User.find();
+
+    for (const user of users) {
+        const profile = faker.helpers.createCard();
+        user.email = `${profile.username}.${profile.email}`;
+        await user.save();
+    }
+};
+
+(async () => {
+    connection = await Connection;
+
+    logger.info('Updating user emails');
+    await updateEmailAddress();
+
+    logger.info('Seeding complete');
+    await connection.close();
+})();
--- cli/seed.passwords.ts
+++ cli/seed.passwords.ts
@@ -1,23 +1,17 @@
-import { getCustomRepository, Connection as typeConnection } from 'typeorm';
+import { Connection as typeConnection } from 'typeorm';
 
 import { Connection } from '../app/services/Connection.service';
-import UserRepository from '../app/repository/User.repository';
 import { hash } from '../app/utils/hash';
 import logger from '../app/utils/logger';
 import User from '../app/models/User';
 
 let connection: typeConnection;
 
-// Converts all user passwords to "secret" (for development use)
 const updateUserPasswords = async () => {
     const users = await User.find();
 
     for (const user of users) {
-        const userRepository = getCustomRepository(UserRepository);
-
-        await userRepository.findByUsername(user.username);
         user.password = await hash('secret');
-
         await user.save();
     }
 };
