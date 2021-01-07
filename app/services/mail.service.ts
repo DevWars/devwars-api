bcfd70e0bcdf6dfb3bc4d800d1cb8e5deb3be00c
Kim Simonsen <polydecay@users.noreply.github.com>
2021-01-07 10:55:57 +0100

    fix: Move email templates out from app directory

--- app/services/mail.service.ts
+++ app/services/mail.service.ts
@@ -1,9 +1,8 @@
 // tslint:disable no-var-requires
-import * as fs from 'fs';
+import * as fs from 'fs/promises';
 import * as path from 'path';
 import * as createMailgun from 'mailgun-js';
 import { getCustomRepository } from 'typeorm';
-
 import GameApplication from '../models/gameApplication.model';
 import User from '../models/user.model';
 
@@ -12,7 +11,13 @@ import logger from '../utils/logger';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const mjml2html = require('mjml');
-const mjmlOptions = { minify: true, keepComments: false };
+
+async function renderTemplate(fileName: string): Promise<any> {
+    const filePath = path.resolve(__dirname, '../../templates', fileName);
+    const template = (await fs.readFile(filePath)).toString();
+
+    return mjml2html(template, { minify: true, keepComments: false, filePath });
+}
 
 export async function send(to: string, subject: string, html: string): Promise<any> {
     // We don't care about anything going on in testing, so just return out early before we attempt
@@ -39,10 +44,7 @@ export async function send(to: string, subject: string, html: string): Promise<a
 
 export async function sendWelcomeEmail(user: User, verificationUrl: string): Promise<void> {
     const subject = 'Welcome to DevWars';
-
-    const filePath = path.resolve(__dirname, '../mail/welcome.mjml');
-    const template = fs.readFileSync(filePath).toString();
-    const output = mjml2html(template, { ...mjmlOptions, filePath });
+    const output = await renderTemplate('welcome.mjml');
 
     // prettier-ignore
     output.html = output.html
@@ -67,10 +69,7 @@ export async function sendGameApplicationApplyingEmail(gameApplication: GameAppl
     }
 
     const subject = 'DevWars Game Application';
-
-    const filePath = path.resolve(__dirname, '../mail/game-application.mjml');
-    const template = fs.readFileSync(filePath).toString();
-    const output = mjml2html(template, { ...mjmlOptions, filePath });
+    const output = await renderTemplate('game-application.mjml');
 
     // prettier-ignore
     output.html = output.html
@@ -96,10 +95,7 @@ export async function SendGameApplicationResignEmail(gameApplication: GameApplic
     }
 
     const subject = 'DevWars Game Application Update (Resign)';
-
-    const filePath = path.resolve(__dirname, '../mail/game-application-resign.mjml');
-    const template = fs.readFileSync(filePath).toString();
-    const output = mjml2html(template, { ...mjmlOptions, filePath });
+    const output = await renderTemplate('game-application-resign.mjml');
 
     // prettier-ignore
     output.html = output.html
@@ -113,9 +109,7 @@ export async function SendGameApplicationResignEmail(gameApplication: GameApplic
 export async function sendPasswordResetEmail(user: User, resetUrl: string) {
     const subject = 'DevWars Password Reset';
 
-    const filePath = path.resolve(__dirname, '../mail/reset-password.mjml');
-    const template = fs.readFileSync(filePath).toString();
-    const output = mjml2html(template, { ...mjmlOptions, filePath });
+    const output = await renderTemplate('reset-password.mjml');
 
     // prettier-ignore
     output.html = output.html
@@ -127,10 +121,7 @@ export async function sendPasswordResetEmail(user: User, resetUrl: string) {
 
 export async function sendContactUsEmail(name: string, email: string, message: string): Promise<void> {
     const subject = `DevWars Contact Us - ${name}`;
-
-    const filePath = path.resolve(__dirname, '../mail/contact-us.mjml');
-    const template = fs.readFileSync(filePath).toString();
-    const output = mjml2html(template, { ...mjmlOptions, filePath });
+    const output = await renderTemplate('contact-us.mjml');
 
     // prettier-ignore
     output.html = output.html
@@ -156,10 +147,7 @@ export async function SendLinkedAccountEmail(user: User, provider: string): Prom
     }
 
     const subject = 'DevWars Linked Account Update';
-
-    const filePath = path.resolve(__dirname, '../mail/linked-account.mjml');
-    const template = fs.readFileSync(filePath).toString();
-    const output = mjml2html(template, { ...mjmlOptions, filePath });
+    const output = await renderTemplate('linked-account.mjml');
 
     // prettier-ignore
     output.html = output.html
@@ -187,10 +175,7 @@ export async function SendUnLinkedAccountEmail(user: User, provider: string): Pr
     }
 
     const subject = 'DevWars Linked Account Update (Unlinked)';
-
-    const filePath = path.resolve(__dirname, '../mail/linked-account-disconnect.mjml');
-    const template = fs.readFileSync(filePath).toString();
-    const output = mjml2html(template, { ...mjmlOptions, filePath });
+    const output = await renderTemplate('linked-account-disconnect.mjml');
 
     // prettier-ignore
     output.html = output.html
